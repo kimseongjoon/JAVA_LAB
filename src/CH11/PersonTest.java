a750a1165fe0ff2ee654fc46d16a8b509579605a
ksj_1016@naver.com <rlatjdwns1>
2020-07-20 15:22:21 +0900

    class 변수 사용 추가

--- src/CH11/PersonTest.java
+++ src/CH11/PersonTest.java
@@ -1,8 +1,12 @@
 package CH11;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 class Person {
-    private String name;
-    private int age;
+    public String name;
+    public int age;
 
     // 생성자 2개, 1.기본, 2.name초기화
     public Person() {
@@ -28,18 +32,36 @@ class Person {
         this.age = age;
     }
 }
+
 public class PersonTest {
     public static void main(String[] args) throws ClassNotFoundException {
         Person person = new Person();
         Class pClass1 = person.getClass();
         System.out.println(pClass1.getName());
 
+//        Class pClass2 = Person.class;
+//        System.out.println(pClass2.getName());
+//
+//        Class pClass3 = Class.forName("CH11.Person");
+//        System.out.println(pClass3.getName());
 
-        Class pClass2 = Person.class;
-        System.out.println(pClass2.getName());
+        Constructor[] cons = pClass1.getConstructors();
+        for (Constructor con : cons) {
+            System.out.println(con);
+        }
 
+        System.out.println();
 
-        Class pClass3 = Class.forName("CH11.Person");
-        System.out.println(pClass3.getName());
+        Field[] fields = pClass1.getFields();
+        for (Field f : fields) {
+            System.out.println(f);
+        }
+
+        System.out.println();
+
+        Method[] methods = pClass1.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method);
+        }
     }
 }
