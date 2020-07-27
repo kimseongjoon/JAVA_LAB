ee5432e060594a6f240552b157dbc979346df9d9
ksj_1016@naver.com <rlatjdwns1>
2020-07-27 10:30:02 +0900

    InnerTest.java 추가

---
+++ src/CH13/innerclass/InnerTest.java
@@ -0,0 +1,33 @@
+package CH13.innerclass;
+
+class OutClass { // 내부클래스 외부의 클래스
+    private int num = 10; // 인스턴스 변수
+    private static int sNum = 20; // 스태틱 변수 (객체없이 사용)
+
+    private InClass inClass; // 내부클래스의 자료형 객체 변수
+
+    public OutClass() { // 외부 클래스 만들시에 인클래스 객체 자동 생성
+        inClass = new InClass();
+    }
+
+    public class InClass { // 내부 클래스
+        int inNum = 100; // 내부클래스의 인스턴스 변수
+        //static int sInNum = 200;
+
+        void inTest() {
+            System.out.println("OutClass num = " + num + "(외부클래스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부클래스 정적변수)");
+        }
+    }
+
+    public void usingClass() {
+        inClass.inTest();
+    }
+}
+public class InnerTest {
+    public static void main(String[] args) {
+        OutClass outClass = new OutClass();
+        System.out.println("외부클래스 이용하여 내부클래스 기능 호출");
+        outClass.usingClass();
+    }
+}
