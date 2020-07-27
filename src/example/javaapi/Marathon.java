b83baa4f35020c3e108b4795d98c6106918ca2ab
ksj_1016@naver.com <rlatjdwns1>
2020-07-27 09:52:12 +0900

    getOrDefault 사용 추가

--- src/example/javaapi/Marathon.java
+++ src/example/javaapi/Marathon.java
@@ -13,23 +13,32 @@ public class Marathon {
         String answer = "";
         HashMap<String, Integer> hm = new HashMap<>();
 
-        for (String name : participant) {
-            if (hm.get(name) == null) {
-                hm.put(name, 1);
-            }
-            else {
-                int value = hm.get(name) + 1;
-                hm.put(name, value);
-            }
+//        for (String name : participant) {
+//            if (hm.get(name) == null) {
+//                hm.put(name, 1);
+//            }
+//            else {
+//                int value = hm.get(name) + 1;
+//                hm.put(name, value);
+//            }
+//        }
+        for (String player : participant) {
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
         }
 
-        for (String name : completion) {
-            int value = hm.get(name) - 1;
-            hm.put(name, value);
+//        for (String name : completion) {
+//            int value = hm.get(name) - 1; // 키값으로 value값 가져옴
+//            hm.put(name, value); // 해시맵에 입력
+//        }
+
+        for (String player : completion) {
+            hm.put(player, hm.getOrDefault(player, 0) - 1);
         }
 
-        for (String name : hm.keySet()) {
-            if (hm.get(name) == 1) answer = name;
+
+        // 마지막에 이름에 해당값이 1인 사람을 찾습니다. (참가는 했지만 완주안함)
+        for (String name : hm.keySet()) { // 해시맵 요소를 전체 반복
+            if (hm.get(name) == 1) answer = name; //
         }
 
         return answer;
